880c3e7ef5846c02537cdc21e1b61ac7485405db
Shreyas Ajith <a.shreyas92@gmail.com>
2019-10-04 13:13:07 +1000

    Intial Commit

---
+++ Paycompute.Services/IEmployeeService.cs
@@ -0,0 +1,33 @@
+﻿using Paycompute.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paycompute.Services
+{
+    public interface IEmployeeService
+    {
+
+        Task CreateAsync(Employee newEmployee);
+
+        Employee GetByID(int employeeId);
+
+        Task UpdateAsync(Employee employee);
+
+        Task UpdateAsync(int id);
+
+        Task Delete(int employeeId);
+
+        decimal UnionFees(int id);
+
+        decimal StudentLoanRepaymentAmount(int id,  decimal totalAmount);
+
+        IEnumerable<Employee> GetAll();
+
+
+
+
+
+    }
+}
